524b72cc00b7b51ef39907fe0c36c21801f28791
pgt27 <2512023305@student.hcmus.edu.vn>
2025-11-17 21:39:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from src.ui.chat_ui import main
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_dir)
+
+from src.ui.chat_ui import main_ui 
 
 if __name__ == "__main__":
-    main()
+    main_ui()
